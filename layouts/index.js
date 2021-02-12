895d345aeb699b51b394d6eba3a34b6867607eae
Stephen Mizell <smizell@gmail.com>
2021-02-12 12:52:32 -0600

    Fix meta data

--- layouts/index.js
+++ layouts/index.js
@@ -1,10 +1,32 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
-export default function Layout({ children }) {
+export default function Layout({ children, frontMatter }) {
   return <div className="page">
+    <>
+      <Head>
+        <title>{frontMatter.title}</title>
+        <meta
+          name="description"
+          content={frontMatter.description}
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@Stephen_Mizell" />
+        <meta name="og:url" content="https://present.smizell.com/" />
+        <meta
+          name="og:title"
+          content={frontMatter.title}
+        />
+        <meta
+          name="og:description"
+          content={frontMatter.description}
+        />
+      </Head>
+    </>
     <nav>
       <Link href="/"><a>API Developer Portal</a></Link>
     </nav>
+    <h2>{frontMatter.title}</h2>
     {children}
   </div>
 }
